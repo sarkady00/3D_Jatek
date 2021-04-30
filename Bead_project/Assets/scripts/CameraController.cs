1ce2b3caade58cf81c82a7086378015aea91f0f7
sarkady00 <sarkadi.blazs@gmail.com>
2021-04-30 20:41:39 +0200

    Karakter modell
    karakter modell, mozgás animációk, RPG-s lett a kamera nézet és a mozgás
    
    Új bug: a karakterre nem sikerült rátenni a materialt.

--- Bead_project/Assets/scripts/CameraController.cs
+++ Bead_project/Assets/scripts/CameraController.cs
@@ -23,20 +23,22 @@ public class CameraController : MonoBehaviour
         }
 
         pivot.transform.position = target.transform.position; // elhelyezzük ugyan oda ahol a karakter van
-        pivot.transform.parent = target.transform; // megadjuk hogy a karakter child komponense legyen
+        pivot.transform.parent = null; // már nem a kamera child-ja
 
         Cursor.lockState = CursorLockMode.Locked; // középre zárjuk az egeret és eltüntetjük
 
     }
 
     // Update is called once per frame
-    void LateUpdate() // update után mozgatjuk a kamerát amikor már tudjuk hol lesz a játékos
+    void LateUpdate() // update után mozgatjuk a kamerát amikor már tudjuk hol lesz a karakter
     {
-        // Lekérjük az egér  x helyzetét és forgatjuk a karaktert
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.Rotate(0,horizontal,0);
+        pivot.transform.position = target.transform.position;
         
-        //ugyan ez y-ra, de csak a kamerát forgatjuk
+        // lekérjük az egér helyzetét és forgatjuk a kamerát
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivot.Rotate(0,horizontal,0);
+        
+        //ugyan ez y-ra
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
         
         if (invertCameraY) // meg adjuk az esélyt hogy ha valakinek úgy jobb akkor legyen invert camera nézet is
@@ -58,14 +60,12 @@ public class CameraController : MonoBehaviour
             pivot.rotation = Quaternion.Euler(360 + minViewAngle,0,0);
         }
 
-        float desiredYAngle = target.eulerAngles.y;
+        float desiredYAngle = pivot.eulerAngles.y;
         float desiredXAngle = pivot.eulerAngles.x;
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle,0);
         transform.position = target.position - (rotation * offset); // a kamerát is a karakternek megfelelően forgatjuk
-        
-        //transform.position = target.position-offset; // a kamerát a karakterhez rögzítjük
 
-        if (transform.position.y < target.position.y)
+        if (transform.position.y < target.position.y) // ne menjen a talaj alá a kamera
         {
             transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z);
         }
--- Bead_project/Assets/scripts/PlayerController.cs
+++ Bead_project/Assets/scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public float gravityScale;
 
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+    public GameObject playerModel;
+
     private Vector3 moveDirection;
         
     // Start is called before the first frame update
@@ -20,13 +25,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
         float yStore = moveDirection.y; // elmentjük az y irányú komponenst még a normalizálás előtt
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore; // visszaadjuk neki azt az értéket
-        if (controller.isGrounded)
+        if (controller.isGrounded) // ha a öldön vagyunk akkor tudunk ugrani
         {
             moveDirection.y = 0f;
             if (Input.GetButtonDown("Jump"))
@@ -36,10 +40,18 @@ public class PlayerController : MonoBehaviour
         }
         
 
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime); // visszaessen a földre
 
-        controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime); // végre hajtuk a mozgást
 
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) // ha nem arra nézünk mint amerre a karakter
+        {
+            transform.rotation = Quaternion.Euler(0, pivot.rotation.eulerAngles.y, 0); // ha valamerre nézünk, és elindulunk akkor a karakter irányba áll és megindul
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
 
+        anim.SetBool("IsGrounded", controller.isGrounded);
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
     }
 }

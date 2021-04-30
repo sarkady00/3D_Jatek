b7c9455e8f08df3d4b7e497258b609d4c46b3ec8
sarkady00 <sarkadi.blazs@gmail.com>
2021-05-01 00:19:49 +0200

    Health system
    Sérülés tereptárgyaktól, gyógyulás

---
+++ Bead_project/Assets/scripts/HeathManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeathManager : MonoBehaviour
+{
+
+    public int maxHealth;
+
+    public int currentHealth;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtPlayer(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void HealPlayer(int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
